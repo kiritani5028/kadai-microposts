a0bf0d39a8c27b7ceb9d908b93577d30c0842123
sho.kiritani <kiritani5038@icloud.com>
2021-03-16 07:56:49 +0000

    favorites

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,17 @@ class User < ApplicationRecord
         uniqueness: { case_sensitive: false }
     has_secure_password
     
+    has_many :favorites
+    has_many :like_microposts, through: :favorites, source: :micropost
+    
     has_many :microposts
     
     has_many :relationships
     has_many :followings, through: :relationships, source: :follow
     has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
     has_many :followers, through: :reverses_of_relationship, source: :user
+    
+    
     
     def follow(other_user)
       unless self == other_user
@@ -30,5 +35,18 @@ class User < ApplicationRecord
     
     def feed_microposts
       Micropost.where(user_id: self.following_ids + [self.id])
+    end
+    
+    def favorite(micropost)
+      self.favorites.find_or_create_by(micropost_id: micropost.id)
+    end
+    
+    def unfavorite(micropost)
+      favorite = self.favorites.find_by(micropost_id: micropost.id)
+      favorite.destroy if favorite
+    end
+    
+    def favorite?(micropost)
+      self.like_microposts.include?(micropost)
     end
 end
